f88125b807e747b01e03863d3e047eb7352660e9
EGuzman16 <114143711+EGuzman16@users.noreply.github.com>
2024-10-17 15:18:19 +0200

    Update AboutPage.jsx

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -5,7 +5,6 @@ import Seccion1 from './container/Seccion1';
 import Seccion2 from './container/Seccion2';
 import Seccion3 from './container/Seccion3';
 import Seccion4 from './container/Seccion4';
-import HorizontalCard from '../../components/HorizontalCard';
 
 const AboutPage = () => {
     return <MainLayout>
@@ -13,8 +12,7 @@ const AboutPage = () => {
         <Seccion1 />
         <Seccion2 />
         <Seccion3 />
-        <Seccion4 />
-        <HorizontalCard />
+        <Seccion4 />     
     </MainLayout>
 };
 
