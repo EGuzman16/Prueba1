4d5ffcf1d9cac3a5d5dc0843eb4c7cd8d434179e
EGuzman16 <16.guzman.elizabeth@gmail.com>
2024-10-20 19:28:41 +0200

    Arreglo de Vista detalle de experiencia

--- src/pages/experienceDetail/container/Main.jsx
+++ src/pages/experienceDetail/container/Main.jsx
@@ -3,6 +3,7 @@ import { AiFillStar } from "react-icons/ai"
 import { LuMapPin } from "react-icons/lu"
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import CommentsContainer from '../../../components/comments/CommentsContainer';
+import postProfile from '../../../assets/post-profile.png';
 
 const Main = () => {
     const [liked, setLiked] = useState(false);
@@ -13,6 +14,8 @@ const Main = () => {
 
     const data = {
         title: "TITULO DEL ATRACTIVO/HOTEL/RESTAURANTE",
+        author: "Nombre del Autor",
+        authorAvatar: postProfile,
         rating: 5,
         prefecture: "NOMBRE DE LA PREFECTURA",
         description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
@@ -20,8 +23,8 @@ const Main = () => {
 
     return (
         <div className="relative">
-            <div className="flex flex-col md:flex-row md:justify-between md:items-center mb-2">
-                <h2 className='text-3xl'>{data.title}</h2>
+            <div className="flex  md:flex-row md:justify-between md:items-center mb-3">
+                <h2 className='text-3xl mb-3'>{data.title}</h2>
                 <div 
                     className="p-2 rounded-full cursor-pointer mt-2 md:mt-0 w-12 h-12 flex items-center justify-center" 
                     style={{ 
@@ -33,6 +36,10 @@ const Main = () => {
                 >
                     {liked ? <FaHeart color="white" /> : <FaRegHeart color="white" />}
                 </div>
+            </div>
+            <div className="flex items-center mb-2">
+                <img src={data.authorAvatar} alt="Autor" className="w-10 h-10 rounded-full mr-2" />
+                <span>{data.author}</span>
             </div>
             <div className="flex mb-2">
                 {Array.from({ length: data.rating }, (_, index) => (
